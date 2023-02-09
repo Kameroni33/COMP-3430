7a8005048c84e56ac9a3f0d2e24a1f330bb52fb1
kameron <khr675@gmail.com>
2023-02-09 11:13:47 -0600

    assignment 1 - feb. 09, 2023

--- assignment1/a1q1.c
+++ assignment1/a1q1.c
@@ -23,7 +23,7 @@ enum e_ident_pos {
 	EI_PAD6
 };
 
-struct elf_header_t {
+struct elf_file_header_t {
 	uint8_t e_ident[16];
 	uint16_t e_type;
 	uint16_t e_machine;
@@ -40,6 +40,32 @@ struct elf_header_t {
 	uint16_t e_shstrndx;
 };
 
+struct elf_program_header_t {
+	uint32_t p_type;
+	uint32_t p_flags;
+	uint64_t p_offset;
+	uint64_t p_vaddr;
+	uint64_t p_paddr;
+	uint64_t p_filesz;
+	uint64_t p_memsz;
+	uint32_t p_flags;
+	uint64_t p_align;
+};
+
+struct elf_section_header_t {
+	uint32_t sh_name;
+	uint32_t sh_type;
+	uint32_t sh_type;
+	uint64_t sh_flags;
+	uint64_t sh_addr;
+	uint64_t sh_offset;
+	uint64_t sh_size;
+	uint32_t sh_link;
+	uint32_t sh_info;
+	uint64_t sh_addralign;
+	uint64_t sh_entsize;
+};
+
 
 int main(int argc, char** argv) {
 
@@ -48,12 +74,15 @@ int main(int argc, char** argv) {
 	FILE* f_ptr;
 
 	// file header variables
-	struct elf_header_t f_header;
+	struct elf_file_header_t f_header;
 	char endian[10];  // need at least 7 characters for 'endian\0'
 	uint8_t bit;  // integer should be 32 or 64, so 8 bits is fine
 
 	// file section header variables
-	// ...
+	struct elf_program_header_t p_header;
+
+	// file section header variables
+	// struct elf_section_header_t s_header;
 
 
 	if (argc > 1) {
@@ -96,7 +125,7 @@ int main(int argc, char** argv) {
 		exit(0);
 	}
 
-
+	// print out header information
 	printf("ELF Header:\n");
 	printf(" * %d-bit\n", bit);
 	printf(" * %s endian\n", endian);
@@ -110,82 +139,13 @@ int main(int argc, char** argv) {
 	printf(" * there are %d section headers, each is %d bytes\n", f_header.e_shnum, f_header.e_shentsize);
 	printf(" * the section header string table is entry %d\n\n", f_header.e_shstrndx);
 
-	// // byte 0x05 determines endianness
-	// char f_endian = fgetc(f_ptr);
-	// char f_endianstr[8];
-    //     if (f_endian == 1) {
-    //             strcpy(f_endianstr, "little");
-    //     } else if (f_endian == 2) {
-    //             strcpy(f_endianstr, "big");
-    //     } else {
-    //             strcpy(f_endianstr, "unknown");
-    //     }
+	// move to program header offset
+	fseek(f_ptr, f_header.e_phoff, SEEK_SET);
 
-	// // byte 0x07 determines operating system ABI
-	// fseek(f_ptr, 1, SEEK_CUR);
-	// char f_abi = fgetc(f_ptr);
+	for (int section = 0; section < f_header.e_phnum; section++) {
 
-	// // bytes 0x10 and 0x11 determine object file type
-	// fseek(f_ptr, 8, SEEK_CUR);
-	// short* f_objtype = malloc(sizeof(short));
-	// fread(f_objtype, 2, 1, f_ptr);
-	
-	// // bytes 0x12 and 0x13 determine instruction set architecture (isa)
-	// short* f_isa = malloc(sizeof(short));
-	// fread(f_isa, 2, 1,f_ptr);
-
-	// // bytes 0x18 to 0x1F hold the memory address of the entry point
-	// fseek(f_ptr, 4, SEEK_CUR);
-	// long* f_entrypoint = malloc(sizeof(long));
-	// fread(f_entrypoint, 8, 1, f_ptr);
-
-	// // bytes 0x20 to 0x27 are the memory address of the program header table
-	// long* f_programheader = malloc(sizeof(long));
-	// fread(f_programheader, 8, 1, f_ptr);
-
-	// // bytes 0x28 to 0x2F are memory address of the section header table
-	// long* f_sectionheader = malloc(sizeof(long));
-	// fread(f_sectionheader, 8, 1, f_ptr);
-
-	// // bytes 0x36 and 0x37 contain the size of a program header table entry
-	// fseek(f_ptr, 6, SEEK_CUR);
-	// short* f_programheader_size = malloc(sizeof(short));
-	// fread(f_programheader_size, 2, 1, f_ptr);
-
-	// // bytes 0x38 and 0x39 contain the number of entries in the program header table
-	// short* f_programheader_num = malloc(sizeof(short));
-	// fread(f_programheader_num, 2, 1, f_ptr);
-
-	// // bytes 0x3A and 0x3B contain the size of a section header table entry
-	// short* f_sectionheader_size = malloc(sizeof(short));
-	// fread(f_sectionheader_size, 2, 1, f_ptr);
-
-	// // bytes 0x3C and 0x3D conatin the number of entries in the section header table
-	// short* f_sectionheader_num = malloc(sizeof(short));
-	// fread(f_sectionheader_num, 2, 1, f_ptr);
-
-	// // bytes 0x3E and 0X3F contain the index of the string table
-	// short* f_stringtable = malloc(sizeof(short));
-	// fread(f_stringtable, 2, 1, f_ptr);
-
-	// printf("ELF Header:\n"
-	// 	" * 64-bit\n"  // since we have not exited execution, we can assume the file is 64-bit
-	// 	" * %s endian\n"
-	// 	" * compiled for 0x%02d (operating system)\n"
-	// 	" * object type is 0x%02d\n"
-	// 	" * compiled for 0x%02d (isa)\n"
-	// 	" * entry point address is 0x%016lu\n"
-	// 	" * program header table starts at 0x%016lu\n"
-	// 	" * section header table starts at 0x%016lu\n"
-	// 	" * there are %d program headers, each is %d bytes\n"
-	// 	" * there are %d section headers, each is %d bytes\n"
-	// 	" * the section header string table is entry %d\n\n",
-	// 	f_endianstr, f_abi, *f_objtype, *f_isa, *f_entrypoint, *f_programheader, *f_sectionheader,
-	// 	*f_programheader_num, *f_programheader_size, *f_sectionheader_num, *f_sectionheader_size,
-	// 	*f_stringtable);
-
-	// // print out information about each program header
-	// for (int i = 0; i < *f_programheader_num; i++) {
+		// read header data into struct
+		fread(&p_header, sizeof(char), sizeof(p_header), f_ptr);
 		
 	// 	// bytes 0x00 to 0x03 identifies the type of segment
 	// 	fseek(f_ptr, (*f_programheader + (*f_programheader_size * i)), SEEK_SET);
@@ -211,12 +171,11 @@ int main(int argc, char** argv) {
 	// 		displayNumBytes = *sizeOnFileImage;
 	// 	}
 
-	// 	printf("Program header #%d:\n"
-	// 		" * segment type 0x%08x\n"
-	// 		" * virtual address of segment 0x%016lx\n"
-	// 		" * size in file %lu bytes\n"
-	// 		" * first %d bytes starting at file offset 0x%016lx:\n",
-	// 		i, *segmentType, *virtualAddress, *sizeOnFileImage, displayNumBytes, *segOffset);
+		printf("Program header #%d:\n", section);
+		printf(" * segment type %#010x\n", p_header.p_type);
+		printf(" * virtual address of segment %018lx\n", p_header.p_vaddr);
+		printf(" * size in file %lu bytes\n", p_header.p_filesz);
+		printf(" * first %d bytes starting at file offset %#018lx:\n", p_header.p_offset);
 		
 	// 	// print first (up to) 32 bytes of file
 	// 	fseek(f_ptr, *segOffset, SEEK_SET);
@@ -236,7 +195,7 @@ int main(int argc, char** argv) {
 	// 	free(segOffset);
 	// 	free(virtualAddress);
 	// 	free(sizeOnFileImage);
-	// }
+	}
 
 
 	// close file
