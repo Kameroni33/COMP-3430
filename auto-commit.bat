3317d208812a6f6530c7d3f0ac958f95771ccd07
kameron <khr675@gmail.com>
2023-02-14 14:39:41 -0600

    (auto-commit) COMP 3430 - 14:39:41.57

---
+++ auto-commit.bat
@@ -0,0 +1,25 @@
+@echo off
+
+rem Message to be included with the auto-commit
+set message=COMP 3430
+
+:loop
+    rem Check if any files have been updated
+    git diff --quiet HEAD
+
+    rem If the exit code is 1, there are files that have been updated
+    if %errorlevel% == 1 (
+        rem Add all changes to the repository (including new files)
+        git add -A
+
+        rem Commit the changes with a timestamp
+        git commit -m "(auto-commit) %message% - %time%"
+
+        rem Push the changes to the remote repository
+        git push
+    )
+
+    rem Wait for a (time in seconds) before checking again
+    timeout /t 5
+
+goto loop
