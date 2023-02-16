201bae3dc970dafeef867052a455397808a833cb
kameron <khr675@gmail.com>
2023-02-15 18:40:20 -0600

    COMP 3430 - lab 3 (auto-commit at 18:40:20.19)

--- lab3/pipeline.c
+++ lab3/pipeline.c
@@ -12,32 +12,27 @@ int main() {
     char *args2[] = {"/bin/tr", "'a-z'", "'A-Z'", NULL};  // process(2) arguments
     char *args3[] = {"/bin/head", "-n", "5", NULL};       // process(3) arguments
 
-    // create pipeline
-    if (pipe(pipes[0]) < 0) {
-        printf("pipe error\n\n");
-        exit(0);
-    }
-
     // create new process
     if ((pid = fork()) < 0) {
         printf("fork error\n\n");
         exit(0);
     }
 
-    // Process 1 ==================================================================================
+    // Parent Process =============================================================================
     else if (pid > 0) {
         dup2(pipes[0][1], STDOUT_FILENO);  // link standard output to pipeline(1) 'write'
         close(pipes[0][0]);                // close pipeline(1) 'read' as we don't need it
         execv("/bin/cat", args1);          // execute process(1)
     }
-
+    
     else {
 
-        // create a new pipeline
-        if (pipe(pipes[1]) < 0) {
+        // create pipeline
+        if (pipe(pipes[0]) < 0) {
             printf("pipe error\n\n");
             exit(0);
         }
+
 
         // create new process
         if ((pid = fork()) < 0) {
@@ -45,21 +40,42 @@ int main() {
             exit(0);
         }
 
-        // Process 2 ==============================================================================
+        // Child Process 1 ========================================================================
         else if (pid > 0) {
-            dup2(pipes[0][0], STDIN_FILENO);   // link standard input to pipeline(1) 'read'
-            close(pipes[0][1]);                // close pipeline(1) 'write' as we don't need it
-            dup2(pipes[1][1], STDOUT_FILENO);  // link standard output to pipeline(2) 'write'
-            close(pipes[1][0]);                // close pipeline(2) 'read' as we don't need it
-            execv("/bin/tr", args2);           // execute process(2)
+            dup2(pipes[0][1], STDOUT_FILENO);  // link standard output to pipeline(1) 'write'
+            close(pipes[0][0]);                // close pipeline(1) 'read' as we don't need it
+            execv("/bin/cat", args1);          // execute process(1)
         }
 
-        // Process 3 ==============================================================================
         else {
-            dup2(pipes[1][0], STDIN_FILENO);   // link standard input to pipeline(2) 'read'
-            close(pipes[1][1]);                // close pipeline(2) 'write' as we don't need it
-            execv("/bin/head", args3);         // execute process(3)
+
+            // create a new pipeline
+            if (pipe(pipes[1]) < 0) {
+                printf("pipe error\n\n");
+                exit(0);
+            }
+
+            // create new process
+            if ((pid = fork()) < 0) {
+                printf("fork error\n\n");
+                exit(0);
+            }
+
+            // Child Process 2 ====================================================================
+            else if (pid > 0) {
+                dup2(pipes[0][0], STDIN_FILENO);   // link standard input to pipeline(1) 'read'
+                close(pipes[0][1]);                // close pipeline(1) 'write' as we don't need it
+                dup2(pipes[1][1], STDOUT_FILENO);  // link standard output to pipeline(2) 'write'
+                close(pipes[1][0]);                // close pipeline(2) 'read' as we don't need it
+                execv("/bin/tr", args2);           // execute process(2)
+            }
+
+            // Child Process 3 ====================================================================
+            else {
+                dup2(pipes[1][0], STDIN_FILENO);   // link standard input to pipeline(2) 'read'
+                close(pipes[1][1]);                // close pipeline(2) 'write' as we don't need it
+                execv("/bin/head", args3);         // execute process(3)
+            }
         }
     }
-
 }
