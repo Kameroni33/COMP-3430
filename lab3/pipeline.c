1801d6758bd66b3531e01795f719d938e8b51215
kameron <khr675@gmail.com>
2023-02-15 18:07:44 -0600

    COMP 3430 - lab 3 (auto-commit at 18:07:44.24)

--- lab3/pipeline.c
+++ lab3/pipeline.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
 
     printf("File: '%s'.\n\n", f_name);
 
-    char *args1[] = {"/bin/cat", "TEST.md", NULL};           // process(1) arguments
+    char *args1[] = {"/bin/cat", "./test.md", NULL};           // process(1) arguments
     char *args2[] = {"/bin/tr", "'a-z'", "'A-Z'", NULL};  // process(2) arguments
     char *args3[] = {"/bin/head", "-n", "5", NULL};       // process(3) arguments
 
