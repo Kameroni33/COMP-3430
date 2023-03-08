8e7a8d2f2f70d7e1eac3348578abf137077de95e
kameron <khr675@gmail.com>
2023-03-08 15:08:49 -0600

    COMP 3430 - lab 3 (auto-commit at 15:08:49.20)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -6,12 +6,7 @@
 #define NUM_OUTPUTS 27
 #define MAX_WORD 100
 
-// Variables & Structs ================================================================
-
-void helloWorld()
-{
-    printf("hello world\n");
-}
+// Methods ============================================================================
 
 void initializeOutputs(FILE *outputFiles[NUM_OUTPUTS], char *outputPaths[NUM_OUTPUTS])
 {
