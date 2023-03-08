9e502aa2d1cc33d5a94fce7371493658c93df18c
kameron <khr675@gmail.com>
2023-03-08 13:46:15 -0600

    COMP 3430 - lab 3 (auto-commit at 13:46:15.25)

--- assignment2/serial.c
+++ assignment2/serial.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include "a2Utils.h"
 
+#define MAX_WORD 100
+
 int main(int argc, char *argv[]) {
-    printf("argc: %d\nargv: %s\n", argc, argv[0]);
-    helloWorld();
+
+    // Variables
+    FILE *file;
+    char word[MAX_WORD];
+
+    printf("process: %s\nfiles:   %d\n\n", argv[0], argc-1);
+    
+    if (argc > 1)
+    {
+        // iterate through all the file names
+        for (int i = 1; i < argc; i++)
+        {
+            // open file and check for errors
+            if (file = fopen(argv[i], "r") == NULL)
+            {
+                printf("Error: unable to open file '%s'\n", argv[i]);
+                exit(1);
+            }
+
+            
+        }
+    }
+
+    else
+    {
+        printf("Usage: ./serial path/to/files/*\n\n");
+    }
+
+    printf("End of Process.\n");
 }
