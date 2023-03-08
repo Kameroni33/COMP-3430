69c333ea5d3c816ff755c84a40923c73268b8dc1
kameron <khr675@gmail.com>
2023-03-08 14:23:17 -0600

    COMP 3430 - lab 3 (auto-commit at 14:23:17.23)

--- assignment2/a2Utils.h
+++ assignment2/a2Utils.h
@@ -1,3 +1,37 @@
 #include <stdio.h>
 
+typedef struct
+{
+    FILE *a;
+    FILE *b;
+    FILE *c;
+    FILE *d;
+    FILE *e;
+    FILE *f;
+    FILE *g;
+    FILE *h;
+    FILE *i;
+    FILE *j;
+    FILE *k;
+    FILE *l;
+    FILE *m;
+    FILE *n;
+    FILE *o;
+    FILE *p;
+    FILE *q;
+    FILE *r;
+    FILE *s;
+    FILE *t;
+    FILE *u;
+    FILE *v;
+    FILE *w;
+    FILE *x;
+    FILE *y;
+    FILE *z;
+    FILE *other;
+} OutputFiles;
+
 void helloWorld();
+
+void openForAppend(FILE *file, char *path)
+void initializeOutputs(OutputFiles outputs)
