69518cf79ba3f487fa83faa71b6525c051e3e2ee
kameron <khr675@gmail.com>
2023-03-08 15:55:55 -0600

    COMP 3430 - lab 3 (auto-commit at 15:55:55.19)

--- assignment2/a2Utils.h
+++ assignment2/a2Utils.h
@@ -5,7 +5,10 @@
 #define NUM_OUTPUTS 27
 #define MAX_WORD 100
 
+// output file methods
 void initializeOutputs(FILE *outputFiles[NUM_OUTPUTS], char *outputPaths[NUM_OUTPUTS]);
 void closeOutputs(FILE *outputFiles[NUM_OUTPUTS]);
 FILE*  determineOutputFile(FILE *outputFiles[NUM_OUTPUTS], char word[MAX_WORD]);
+
+// logging and timing methods
 long long timeInMilliseconds(void);
