0a94c0aac6d5326d208440141294d59e478b7265
kameron <khr675@gmail.com>
2023-03-08 14:35:53 -0600

    COMP 3430 - lab 3 (auto-commit at 14:35:53.26)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -7,9 +7,6 @@
 
 // Variables & Structs ================================================================
 
-FILE *outputFiles[NUM_OUTPUTS];
-char *outputPaths[NUM_OUTPUTS] = { "output/a.txt", "output/b.txt", "output/c.txt", "output/d.txt", "output/e.txt", "output/f.txt", "output/g.txt", "output/h.txt", "output/i.txt", "output/j.txt", "output/k.txt", "output/l.txt", "output/m.txt", "output/n.txt", "output/o.txt", "output/p.txt", "output/q.txt", "output/r.txt", "output/s.txt", "output/t.txt", "output/u.txt", "output/v.txt", "output/w.txt", "output/x.txt", "output/y.txt", "output/z.txt", "output/other.txt" };
-
 void helloWorld()
 {
     printf("hello world\n");
