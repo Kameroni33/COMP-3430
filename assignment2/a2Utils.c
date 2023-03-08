5e4249c85eff8bfdb4173039348e25620ad745b3
kameron <khr675@gmail.com>
2023-03-08 14:34:17 -0600

    COMP 3430 - lab 3 (auto-commit at 14:34:17.24)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -7,66 +7,8 @@
 
 // Variables & Structs ================================================================
 
-typedef struct
-{
-    FILE *a;
-    FILE *b;
-    FILE *c;
-    FILE *d;
-    FILE *e;
-    FILE *f;
-    FILE *g;
-    FILE *h;
-    FILE *i;
-    FILE *j;
-    FILE *k;
-    FILE *l;
-    FILE *m;
-    FILE *n;
-    FILE *o;
-    FILE *p;
-    FILE *q;
-    FILE *r;
-    FILE *s;
-    FILE *t;
-    FILE *u;
-    FILE *v;
-    FILE *w;
-    FILE *x;
-    FILE *y;
-    FILE *z;
-    FILE *other;
-} OutputFiles;
-
-char *outputNames[] = {
-    "a.txt"
-    "b.txt"
-    "c.txt"
-    "d.txt"
-    "e.txt"
-    "f.txt"
-    "g.txt"
-    "h.txt"
-    "i.txt"
-    "j.txt"
-    "k.txt"
-    "l.txt"
-    "m.txt"
-    "n.txt"
-    "o.txt"
-    "p.txt"
-    "q.txt"
-    "r.txt"
-    "s.txt"
-    "t.txt"
-    "u.txt"
-    "v.txt"
-    "w.txt"
-    "x.txt"
-    "y.txt"
-    "z.txt"
-    "other.txt"
-};
+FILE *outputFiles[NUM_OUTPUTS];
+char *outputPaths[NUM_OUTPUTS] = { "output/a.txt", "output/b.txt", "output/c.txt", "output/d.txt", "output/e.txt", "output/f.txt", "output/g.txt", "output/h.txt", "output/i.txt", "output/j.txt", "output/k.txt", "output/l.txt", "output/m.txt", "output/n.txt", "output/o.txt", "output/p.txt", "output/q.txt", "output/r.txt", "output/s.txt", "output/t.txt", "output/u.txt", "output/v.txt", "output/w.txt", "output/x.txt", "output/y.txt", "output/z.txt", "output/other.txt" };
 
 void helloWorld()
 {
@@ -75,40 +17,18 @@ void helloWorld()
 
 void openForAppend(FILE *file, char *path)
 {
-    if ((file = fopen(path, "a")) == NULL) {
-        printf("Error: unable to open file for append '%s'\n", path);
-        exit(1);
-    }
+    
 }
 
-void initializeOutputs(OutputFiles outputs)
+void initializeOutputs(FILE *outputFiles[])
 {
-    openForAppend(outputs.a, "a.txt");
-    openForAppend(outputs.b, "b.txt");
-    openForAppend(outputs.c, "c.txt");
-    openForAppend(outputs.d, "d.txt");
-    openForAppend(outputs.e, "e.txt");
-    openForAppend(outputs.f, "f.txt");
-    openForAppend(outputs.g, "g.txt");
-    openForAppend(outputs.h, "h.txt");
-    openForAppend(outputs.i, "i.txt");
-    openForAppend(outputs.j, "j.txt");
-    openForAppend(outputs.k, "k.txt");
-    openForAppend(outputs.l, "l.txt");
-    openForAppend(outputs.m, "m.txt");
-    openForAppend(outputs.n, "n.txt");
-    openForAppend(outputs.o, "o.txt");
-    openForAppend(outputs.p, "p.txt");
-    openForAppend(outputs.q, "q.txt");
-    openForAppend(outputs.r, "r.txt");
-    openForAppend(outputs.s, "s.txt");
-    openForAppend(outputs.t, "t.txt");
-    openForAppend(outputs.u, "u.txt");
-    openForAppend(outputs.v, "v.txt");
-    openForAppend(outputs.w, "w.txt");
-    openForAppend(outputs.x, "x.txt");
-    openForAppend(outputs.y, "y.txt");
-    openForAppend(outputs.z, "z.txt");
-    openForAppend(outputs.other, "other.txt");
+    for ( int i = 0; i < NUM_OUTPUTS; i++)
+    {
+        if ((outputFiles[i] = fopen(outputPaths[i], "a")) == NULL)
+        {
+            printf("Error: unable to open file for append '%s'\n", outputPaths[i]);
+            exit(1);
+        }
+    }
 }
 
