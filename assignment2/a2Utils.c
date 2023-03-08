961cc630f49a8994b77f50d75b5333bb6f450c0c
kameron <khr675@gmail.com>
2023-03-08 14:04:08 -0600

    COMP 3430 - lab 3 (auto-commit at 14:04:08.26)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
-void helloWorld() {
+// Global Constants ===================================================================
+
+#define MAX_WORD 100
+
+// Variables & Structs ================================================================
+
+struct OutputFiles
+{
+    FILE *a;
+    FILE *b;
+    FILE *c;
+    FILE *d;
+    FILE *e;
+    FILE *f;
+    FILE *g;
+    FILE *h;
+    FILE *i;
+    FILE *j;
+    FILE *k;
+    FILE *l;
+    FILE *m;
+    FILE *n;
+    FILE *o;
+    FILE *p;
+    FILE *q;
+    FILE *r;
+    FILE *s;
+    FILE *t;
+    FILE *u;
+    FILE *v;
+    FILE *w;
+    FILE *x;
+    FILE *y;
+    FILE *z;
+    FILE *other;
+} output;
+
+void helloWorld()
+{
     printf("hello world\n");
+}
+
+void initializeOutputs(struct output outputs)
+{
+
 }
 
