11bba8a38c5681fa9061ce0889429f004838d85f
kameron <khr675@gmail.com>
2023-03-08 16:50:23 -0600

    COMP 3430 - lab 3 (auto-commit at 16:50:23.20)

--- assignment2/withLocks.c
+++ assignment2/withLocks.c
@@ -1,6 +1,99 @@
-#include <stdio.h>
-#include "a2Utils.h"
+#include <stdio.h>    // fopen, fclose, fwrite, printf, fscanf
+#include <stdlib.h>   // exit
+
+#include "a2Utils.h"  // initializeOutputs
+
+// Global Variables ===================================================================
+
+char *outputPaths[NUM_OUTPUTS] = {
+    "output/a.txt",
+    "output/b.txt",
+    "output/c.txt",
+    "output/d.txt",
+    "output/e.txt",
+    "output/f.txt",
+    "output/g.txt",
+    "output/h.txt",
+    "output/i.txt",
+    "output/j.txt",
+    "output/k.txt",
+    "output/l.txt",
+    "output/m.txt",
+    "output/n.txt",
+    "output/o.txt",
+    "output/p.txt",
+    "output/q.txt",
+    "output/r.txt",
+    "output/s.txt",
+    "output/t.txt",
+    "output/u.txt",
+    "output/v.txt",
+    "output/w.txt",
+    "output/x.txt",
+    "output/y.txt",
+    "output/z.txt",
+    "output/other.txt"
+};
+
+FILE *outputFiles[NUM_OUTPUTS];
+
+long long startTime;
+long long endTime;
+
+// Main Method ========================================================================
 
 int main(int argc, char *argv[]) {
-    printf("argc: %d\nargv: %s\n", argc, argv[0]);
+
+    // Local Variables
+    FILE *inputFile;
+    char word[MAX_WORD];
+
+    startTime = timeInMilliseconds();
+
+    // open all of our output files for 'append'
+    initializeOutputs(outputFiles, outputPaths);
+
+    printf("process: %s\nfiles:   %d\n\n", argv[0]+2, argc-1);
+    
+    if (argc > 1)
+    {
+        // iterate through all the file names
+        for (int i = 1; i < argc; i++)
+        {
+            printf("reading file '%s'\n", argv[i]);
+
+            // open input file and check for errors
+            if ((inputFile = fopen(argv[i], "r")) == NULL)
+            {
+                printf("Error: unable to open file '%s'.\n", argv[i]);
+                exit(1);
+            }
+
+            // scan through entire input file
+            while (fscanf(inputFile, "%s", word) != EOF)
+            {
+                // call our util function to determine which output file to write to and append the current word to it
+                fprintf(determineOutputFile(outputFiles, word), "%s\n", word);
+            }
+
+            // close current input file
+            fclose(inputFile);
+        }
+    }
+
+    else
+    {
+        printf("Error: no input file(s) provided.\n");
+        printf("Usage: ./serial path/to/files/*\n\n");
+    }
+
+    // close all of our output files
+    closeOutputs(outputFiles);
+
+    endTime = timeInMilliseconds();
+
+    logInfo(startTime, endTime, 1, argc, argv);
+
+    printf("End of Process.\n");
+    return 0;
 }
