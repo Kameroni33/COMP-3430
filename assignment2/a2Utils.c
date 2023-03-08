97e27cb42b12807df08aa4fdace66e2f49389a5d
kameron <khr675@gmail.com>
2023-03-08 15:05:43 -0600

    COMP 3430 - lab 3 (auto-commit at 15:05:43.23)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -37,22 +37,25 @@ void closeOutputs(FILE *outputFiles[NUM_OUTPUTS])
     }
 }
 
-void findOutputFile(FILE *outputFiles[NUM_OUTPUTS], char word[MAX_WORD])
+FILE* findOutputFile(FILE *outputFiles[NUM_OUTPUTS], char word[MAX_WORD])
 {
-    printf("first character: %c | ", word[0]);
-    
+    // uppercase ASCII letters range from 65 - 90
     if ( word[0] > 64 && word[0] < 91 )
     {
-        printf("UPPERCASE LETTER\n");
+        // return the correct output file descriptor by offsetting by 65
+        return outputFiles[word[0]-65];
     }
 
+    // lowercase ASCII letters range from 97 - 122
     else if ( word[0] > 96 && word[0] < 123 )
     {
-        printf("lowercase letter\n");
+        // return the correct output file descriptor by offsetting by 97
+        return outputFiles[word[0]-97];
     }
 
     else
     {
-        printf("NeItHeR...\n");
+        // all other characters go the same output file (last one in our list of files)
+        return outputFiles[NUM_OUTPUTS-1];
     }
 }
