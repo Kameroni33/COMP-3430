afdbba4a0303582e1160c761112256ee78ced180
kameron <khr675@gmail.com>
2023-03-08 20:50:23 -0600

    COMP 3430 - lab 3 (auto-commit at 20:50:23.19)

--- assignment2/serial.c
+++ assignment2/serial.c
@@ -3,6 +3,9 @@
 
 #include "a2Utils.h"  // initializeOutputs
 
+// global array of output file descriptors (ordered to match outputPaths)
+FILE *outputFiles[NUM_OUTPUTS];
+
 // timing variables for logging
 long long startTime, endTime;
 
