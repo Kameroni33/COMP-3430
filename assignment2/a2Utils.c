1a88cd670c8a3aafbe10a7698d257d7d95b57f14
kameron <khr675@gmail.com>
2023-03-08 22:58:14 -0600

    COMP 3430 - lab 3 (auto-commit at 22:58:14.21)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -11,6 +11,9 @@
 
 // Global Variables ===================================================================
 
+// timing variables for logging
+long long startTime, endTime;
+
 // global array of output file descriptors (ordered a-z + other)
 char *outputPaths[NUM_OUTPUTS] = { "output/a.txt", "output/b.txt", "output/c.txt", "output/d.txt", "output/e.txt", "output/f.txt", "output/g.txt", "output/h.txt", "output/i.txt", "output/j.txt", "output/k.txt", "output/l.txt", "output/m.txt", "output/n.txt", "output/o.txt", "output/p.txt", "output/q.txt", "output/r.txt", "output/s.txt", "output/t.txt", "output/u.txt", "output/v.txt", "output/w.txt", "output/x.txt", "output/y.txt", "output/z.txt", "output/other.txt" };
 
