e7960457a776e3b88acf355112c7524bc7160913
kameron <khr675@gmail.com>
2023-03-08 20:45:02 -0600

    COMP 3430 - lab 3 (auto-commit at 20:45:02.24)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -3,6 +3,41 @@
 #include <string.h>
 #include <sys/time.h>
 
+// Global Variables ===================================================================
+
+char *outputPaths[NUM_OUTPUTS] = {
+    "output/a.txt",
+    "output/b.txt",
+    "output/c.txt",
+    "output/d.txt",
+    "output/e.txt",
+    "output/f.txt",
+    "output/g.txt",
+    "output/h.txt",
+    "output/i.txt",
+    "output/j.txt",
+    "output/k.txt",
+    "output/l.txt",
+    "output/m.txt",
+    "output/n.txt",
+    "output/o.txt",
+    "output/p.txt",
+    "output/q.txt",
+    "output/r.txt",
+    "output/s.txt",
+    "output/t.txt",
+    "output/u.txt",
+    "output/v.txt",
+    "output/w.txt",
+    "output/x.txt",
+    "output/y.txt",
+    "output/z.txt",
+    "output/other.txt"
+};
+
+// global array of output file descriptors (ordered to match outputPaths)
+FILE *outputFiles[NUM_OUTPUTS];
+
 // Global Constants ===================================================================
 
 #define NUM_OUTPUTS 27  // number of output files (a-z + other)
