9a8413c450c4280bcb96f69de21ea55b57e6c75f
kameron <khr675@gmail.com>
2023-03-10 01:09:54 -0600

    COMP 3430 - lab 3 (auto-commit at  1:09:54.24)

---
+++ assignment2/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in {1..100}
+do
+    ./withLocks $i ~comp3430/shared/a2_files/*.txt
+    ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
+done
+
+# for i in {1..100}
+# do
+#     ./serial ~comp3430/shared/a2_files/*.txt
+#     ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
+#     ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
+# done
