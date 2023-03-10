ae8bf745b08bc5d1911a16bb887ba67e9378a24e
kameron <khr675@gmail.com>
2023-03-10 02:48:07 -0600

    COMP 3430 - lab 3 (auto-commit at  2:48:07.20)

--- assignment2/test.sh
+++ assignment2/test.sh
@@ -16,7 +16,7 @@ rm -f /tmp/ronaldk1/output/* /tmp/ronaldk1/fifo/* /tmp/ronaldk1/log/*
 for i in {1..100}
 do
     ./serial ~comp3430/shared/a2_files/*.txt
-    ./withFIFOs 10 ~comp3430/shared/a2_files/*.txt
+    ./withLocks 10 ~comp3430/shared/a2_files/*.txt
     ./withFIFOs 10 ~comp3430/shared/a2_files/*.txt
     # clean up output files so we don't run out of space
     rm -f /tmp/ronaldk1/output/*
