50dd827656ef145db7d9807966df9b4797c372a1
kameron <khr675@gmail.com>
2023-03-10 02:29:13 -0600

    COMP 3430 - lab 3 (auto-commit at  2:29:13.27)

--- assignment2/test.sh
+++ assignment2/test.sh
@@ -4,18 +4,20 @@
 rm -f /tmp/ronaldk1/output/* /tmp/ronaldk1/fifo/* /tmp/ronaldk1/log/*
 
 # Find optimal number of workers
+# for i in {1..100}
+# do
+#     ./withLocks $i ~comp3430/shared/a2_files/*.txt
+#     ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
+#     # clean up output files so we don't run out of space
+#     rm -f /tmp/ronaldk1/output/*
+# done
+
+# Find average/mean for each program
 for i in {1..100}
 do
-    ./withLocks $i ~comp3430/shared/a2_files/*.txt
-    ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
+    ./serial ~comp3430/shared/a2_files/*.txt
+    ./withFIFOs 10 ~comp3430/shared/a2_files/*.txt
+    ./withFIFOs 10 ~comp3430/shared/a2_files/*.txt
     # clean up output files so we don't run out of space
     rm -f /tmp/ronaldk1/output/*
 done
-
-# Find average/mean for each program
-# for i in {1..100}
-# do
-#     ./serial ~comp3430/shared/a2_files/*.txt
-#     ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
-#     ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
-# done
