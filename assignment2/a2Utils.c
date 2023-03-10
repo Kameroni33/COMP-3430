f6fea2497a39ec579f34c1456a4ed14b86c01212
kameron <khr675@gmail.com>
2023-03-10 01:34:01 -0600

    COMP 3430 - lab 3 (auto-commit at  1:34:01.28)

--- assignment2/a2Utils.c
+++ assignment2/a2Utils.c
@@ -197,7 +197,7 @@ void makeFifos()
     for (int i = 0; i < NUM_OUTPUTS; i++)
     {
         mkfifo((fifoFilePath = getFifoFilePath(i)), S_IRUSR | S_IWUSR);  // make fifo with read/write privilages for user
-        free(fifoFilePath);  // free filename pointer
+        // free(fifoFilePath);  // free filename pointer
     }
 }
 
