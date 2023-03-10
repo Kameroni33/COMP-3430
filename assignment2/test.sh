fec47a6fdb83ba0057825446ba73622deafff5f7
kameron <khr675@gmail.com>
2023-03-10 01:23:29 -0600

    COMP 3430 - lab 3 (auto-commit at  1:23:29.27)

--- assignment2/test.sh
+++ assignment2/test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# clean up any old tests...
+rm -f /tmp/ronaldk1/output/* /tmp/ronaldk1/fifo/* /tmp/ronaldk1/log/*
+
+# Find optimal number of workers
 for i in {1..100}
 do
     ./withLocks $i ~comp3430/shared/a2_files/*.txt
     ./withFIFOs $i ~comp3430/shared/a2_files/*.txt
-    rm -rf 
+    rm -f /tmp/ronaldk1/output/* /tmp/ronaldk1/fifo/*
 done
 
+# Find average/mean for each program
 # for i in {1..100}
 # do
 #     ./serial ~comp3430/shared/a2_files/*.txt
