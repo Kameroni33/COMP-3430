9f05b3f2566d31e752d3f136b7f699dbe34f4298
kameron <khr675@gmail.com>
2023-03-16 19:57:56 -0500

    COMP 3430 - lab 3 (auto-commit at 19:57:56.31)

--- lab4/fsck-fat.c
+++ lab4/fsck-fat.c
@@ -27,7 +27,7 @@ int main(int argc, char* argv[])
         printf("BPB_FATSz32:  %d\n", mbr.BPB_FATSz32);
         printf("BPB_RootClus: %d\n", mbr.BPB_RootClus);
         printf("BPB_TotSec32: %d\n", mbr.BPB_TotSec32);
-        printf("BPB_reserved: %s\n", mbr.BPB_reserved);
+        printf("BPB_reserved: %X %X %X %X %X %X %X %X %X %X %X %X\n", mbr.BPB_reserved[0], mbr.BPB_reserved[1], mbr.BPB_reserved[2], mbr.BPB_reserved[3], mbr.BPB_reserved[4], mbr.BPB_reserved[5], mbr.BPB_reserved[6], mbr.BPB_reserved[7], mbr.BPB_reserved[8], mbr.BPB_reserved[9], mbr.BPB_reserved[10], mbr.BPB_reserved[11]);
 
         printf("\n");
     }
