89a0a2051eac30d63f44a5818718ca0a0a5f4b9c
kameron <khr675@gmail.com>
2023-03-16 19:51:43 -0500

    COMP 3430 - lab 3 (auto-commit at 19:51:43.25)

--- lab4/fsck-fat.c
+++ lab4/fsck-fat.c
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 
         fread(&mbr, sizeof(fat32BS), 1, volume);
 
-        printf("BS_jmpBoot:   %c\n", mbr.BS_jmpBoot[0]);
+        printf("BS_jmpBoot:   %c\n", mbr.BS_jmpBoot);
         printf("BPB_FATSz32:  %d\n", mbr.BPB_FATSz32);
         printf("BPB_RootClus: %d\n", mbr.BPB_RootClus);
         printf("BPB_TotSec32: %d\n", mbr.BPB_TotSec32);
