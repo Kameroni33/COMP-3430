6262d84604bec73cd4796106c6345968c4ef9786
kameron <khr675@gmail.com>
2023-03-17 01:05:38 -0500

    COMP 3430 - lab 3 (auto-commit at  1:05:38.24)

--- lab4/fsck-fat.c
+++ lab4/fsck-fat.c
@@ -111,7 +111,7 @@ int main(int argc, char* argv[])
 
 
         // print free space info
-        printf("FSI says %d free clusters, FAT count is %d", fsInfoSector.free_count, 0);
+        printf("FSI says %d free clusters, FAT count is %d\n", fsInfoSector.free_count, 0);
 
     }
 
