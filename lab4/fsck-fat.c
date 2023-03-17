bee5f7d86775301e70f3c7fd1eb4aa31f43972d5
kameron <khr675@gmail.com>
2023-03-17 00:33:08 -0500

    COMP 3430 - lab 3 (auto-commit at  0:33:08.25)

--- lab4/fsck-fat.c
+++ lab4/fsck-fat.c
@@ -14,10 +14,11 @@ int main(int argc, char* argv[])
         printf("==================================================\n\n");
 
         FILE* volume;
-        fat32BS mbr;
+        fat32BS bootSector;
+        FSInfo fsInfoSector; 
         char FAT[2];
 
-        int validMBR = 1;
+        int validBootSector = 1;
 
         // open .img file
         if ((volume = fopen(argv[i], "r")) == NULL)
@@ -26,67 +27,73 @@ int main(int argc, char* argv[])
             exit(1);
         }
 
-        // read volume into fat32BS struct
-        fread(&mbr, sizeof(fat32BS), 1, volume);
+        // read Boot Sector
+        fread(&bootSector, sizeof(fat32BS), 1, volume);
 
-        // print MBR info
-        printf("BS_jmpBoot:   0x%X 0x%X 0x%X\n", mbr.BS_jmpBoot[0] & 0xff, mbr.BS_jmpBoot[1] & 0xff, mbr.BS_jmpBoot[2] & 0xff);
-        printf("BPB_FATSz32:  %d\n", mbr.BPB_FATSz32);
-        printf("BPB_RootClus: %d\n", mbr.BPB_RootClus);
-        printf("BPB_TotSec32: %d\n", mbr.BPB_TotSec32);
-        printf("BPB_reserved: %X %X %X %X %X %X %X %X %X %X %X %X\n\n", mbr.BPB_reserved[0], mbr.BPB_reserved[1], mbr.BPB_reserved[2], mbr.BPB_reserved[3], mbr.BPB_reserved[4], mbr.BPB_reserved[5], mbr.BPB_reserved[6], mbr.BPB_reserved[7], mbr.BPB_reserved[8], mbr.BPB_reserved[9], mbr.BPB_reserved[10], mbr.BPB_reserved[11]);
+        // print bootSector info
+        printf("BS_jmpBoot:   0x%X 0x%X 0x%X\n", bootSector.BS_jmpBoot[0] & 0xff, bootSector.BS_jmpBoot[1] & 0xff, bootSector.BS_jmpBoot[2] & 0xff);
+        printf("BPB_FATSz32:  %d\n", bootSector.BPB_FATSz32);
+        printf("BPB_RootClus: %d\n", bootSector.BPB_RootClus);
+        printf("BPB_TotSec32: %d\n", bootSector.BPB_TotSec32);
+        printf("BPB_reserved: %X %X %X %X %X %X %X %X %X %X %X %X\n\n", bootSector.BPB_reserved[0], bootSector.BPB_reserved[1], bootSector.BPB_reserved[2], bootSector.BPB_reserved[3], bootSector.BPB_reserved[4], bootSector.BPB_reserved[5], bootSector.BPB_reserved[6], bootSector.BPB_reserved[7], bootSector.BPB_reserved[8], bootSector.BPB_reserved[9], bootSector.BPB_reserved[10], bootSector.BPB_reserved[11]);
 
         // validate BS_jmpBoot
-        if ((int)(mbr.BS_jmpBoot[0] & 0xff) != 0xEB && (int)(mbr.BS_jmpBoot[0] & 0xff) != 0xE9)
+        if ((int)(bootSector.BS_jmpBoot[0] & 0xff) != 0xEB && (int)(bootSector.BS_jmpBoot[0] & 0xff) != 0xE9)
         {
-            printf("Inconsistent file system: BS_jmpBoot[0] must be 0xEB or 0xE9, but is 0x%X.\n", mbr.BS_jmpBoot[0]);
-            validMBR = 0;
+            printf("Inconsistent file system: BS_jmpBoot[0] must be 0xEB or 0xE9, but is 0x%X.\n", bootSector.BS_jmpBoot[0]);
+            validBootSector = 0;
         }
 
         // validate BPB_FATSz32
-        if (mbr.BPB_FATSz32 == 0)
+        if (bootSector.BPB_FATSz32 == 0)
         {
-            printf("Inconsistent file system: BPB_FATSz32 should be non-zero, but is%d.\n", mbr.BPB_FATSz32);
-            validMBR = 0;
+            printf("Inconsistent file system: BPB_FATSz32 should be non-zero, but is%d.\n", bootSector.BPB_FATSz32);
+            validBootSector = 0;
         }
 
         // validate BPB_RootClus
-        if (mbr.BPB_RootClus < 2)
+        if (bootSector.BPB_RootClus < 2)
         {
-            printf("Inconsistent file system: BPB_RootClus should greater than 2, but is %d.\n", mbr.BPB_RootClus);
-            validMBR = 0;
+            printf("Inconsistent file system: BPB_RootClus should greater than 2, but is %d.\n", bootSector.BPB_RootClus);
+            validBootSector = 0;
         }
 
         // validate BPB_TotSec32
-        if (mbr.BPB_TotSec32 < 65525)
+        if (bootSector.BPB_TotSec32 < 65525)
         {
-            printf("Inconsistent file system: BPB_TotSec32 should be greater than 65525, but is %d.\n", mbr.BPB_TotSec32);
-            validMBR = 0;
+            printf("Inconsistent file system: BPB_TotSec32 should be greater than 65525, but is %d.\n", bootSector.BPB_TotSec32);
+            validBootSector = 0;
         }
 
         // validate BPB_reserved
-        if (mbr.BPB_reserved[0] != 0 || mbr.BPB_reserved[1] != 0 || mbr.BPB_reserved[2] != 0 || mbr.BPB_reserved[3] != 0 || mbr.BPB_reserved[4] != 0 || mbr.BPB_reserved[5] != 0 || mbr.BPB_reserved[6] != 0 || mbr.BPB_reserved[7] != 0 || mbr.BPB_reserved[8] != 0 || mbr.BPB_reserved[9] != 0 || mbr.BPB_reserved[10] != 0 || mbr.BPB_reserved[11] != 0)
+        if (bootSector.BPB_reserved[0] != 0 || bootSector.BPB_reserved[1] != 0 || bootSector.BPB_reserved[2] != 0 || bootSector.BPB_reserved[3] != 0 || bootSector.BPB_reserved[4] != 0 || bootSector.BPB_reserved[5] != 0 || bootSector.BPB_reserved[6] != 0 || bootSector.BPB_reserved[7] != 0 || bootSector.BPB_reserved[8] != 0 || bootSector.BPB_reserved[9] != 0 || bootSector.BPB_reserved[10] != 0 || bootSector.BPB_reserved[11] != 0)
         {
-            printf("Inconsistent file system: BPB_reserved should all be 0, but is %X %X %X %X %X %X %X %X %X %X %X %X\n", mbr.BPB_reserved[0], mbr.BPB_reserved[1], mbr.BPB_reserved[2], mbr.BPB_reserved[3], mbr.BPB_reserved[4], mbr.BPB_reserved[5], mbr.BPB_reserved[6], mbr.BPB_reserved[7], mbr.BPB_reserved[8], mbr.BPB_reserved[9], mbr.BPB_reserved[10], mbr.BPB_reserved[11]);
-            validMBR = 0;
+            printf("Inconsistent file system: BPB_reserved should all be 0, but is %X %X %X %X %X %X %X %X %X %X %X %X\n", bootSector.BPB_reserved[0], bootSector.BPB_reserved[1], bootSector.BPB_reserved[2], bootSector.BPB_reserved[3], bootSector.BPB_reserved[4], bootSector.BPB_reserved[5], bootSector.BPB_reserved[6], bootSector.BPB_reserved[7], bootSector.BPB_reserved[8], bootSector.BPB_reserved[9], bootSector.BPB_reserved[10], bootSector.BPB_reserved[11]);
+            validBootSector = 0;
         }
 
         // if all validations passed, then print success
-        if (validMBR)
+        if (validBootSector)
         {
             printf("MBR appears to be consistent.\n");
         }
 
-        // jump to cluster 1
-        fseek(volume, (mbr.BPB_RsvdSecCnt * mbr.BPB_BytesPerSec), SEEK_SET);
+        printf("BPB_BytesPerSec: %d\n", bootSector.BPB_BytesPerSec);
+        printf("BPB_FSInfo:      %d\n", bootSector.BPB_FSInfo);
+        printf("BPB_RsvdSecCnt:  %d\n", bootSector.BPB_RsvdSecCnt);
 
-        // read first 
+        // read File System Info Sector
+        fseek(volume, (bootSector.BPB_FSInfo * bootSector.BPB_BytesPerSec), SEEK_SET);
+        fread(&fsInfoSector, sizeof(FSInfo), 1, volume);
+
+        // read File Allocation Table
+        fseek(volume, (bootSector.BPB_RsvdSecCnt * bootSector.BPB_BytesPerSec), SEEK_SET);
         fread(&FAT, sizeof(char) * 2, 1, volume);
 
         // print FAT info
         printf("\n");
-        printf("FAT[0]: 0x%X\n", mbr.BS_jmpBoot[0]);
-        printf("FAT[0]: 0x%X\n", mbr.BS_jmpBoot[1]);
+        printf("FAT[0]: 0x%X\n", bootSector.BS_jmpBoot[0]);
+        printf("FAT[0]: 0x%X\n", bootSector.BS_jmpBoot[1]);
 
     }
 
