c94bdad46203c9ab36f57f2e697e42fb3f15b580
kameron <khr675@gmail.com>
2023-03-16 23:52:19 -0500

    COMP 3430 - lab 3 (auto-commit at 23:52:19.30)

--- lab4/fsck-fat.c
+++ lab4/fsck-fat.c
@@ -16,6 +16,8 @@ int main(int argc, char* argv[])
         FILE* volume;
         fat32BS mbr = {0};
 
+        int validMBR = 1;
+
         // open .img file
         if ((volume = fopen(argv[i], "r")) == NULL)
         {
@@ -36,38 +38,41 @@ int main(int argc, char* argv[])
         if (mbr.BS_jmpBoot[0] != 0xEB && mbr.BS_jmpBoot[0] != 0xE9)
         {
             printf("Inconsistent file system: BS_jmpBoot[0] must be 0xEB or 0xE9, but is 0x%X.\n", mbr.BS_jmpBoot[0]);
-            break;
+            validMBR = 0;
         }
 
         //validate BPB_FATSz32
         if (mbr.BPB_FATSz32 == 0)
         {
             printf("Inconsistent file system: BPB_FATSz32 should be non-zero, but is%d.\n", mbr.BPB_FATSz32);
-            break;
+            validMBR = 0;
         }
 
         //validate BPB_RootClus
         if (mbr.BPB_RootClus < 2)
         {
             printf("Inconsistent file system: BPB_RootClus should greater than 2, but is %d.\n", mbr.BPB_RootClus);
-            break;
+            validMBR = 0;
         }
 
         //validate BPB_TotSec32
         if (mbr.BPB_TotSec32 < 65525)
         {
             printf("Inconsistent file system: BPB_TotSec32 should be greater than 65525, but is %d.\n", mbr.BPB_TotSec32);
-            break;
+            validMBR = 0;
         }
 
         //validate BPB_reserved
         if (mbr.BPB_reserved[0] != 0 || mbr.BPB_reserved[1] != 0 || mbr.BPB_reserved[2] != 0 || mbr.BPB_reserved[3] != 0 || mbr.BPB_reserved[4] != 0 || mbr.BPB_reserved[5] != 0 || mbr.BPB_reserved[6] != 0 || mbr.BPB_reserved[7] != 0 || mbr.BPB_reserved[8] != 0 || mbr.BPB_reserved[9] != 0 || mbr.BPB_reserved[10] != 0 || mbr.BPB_reserved[11] != 0)
         {
             printf("Inconsistent file system: BPB_reserved should all be 0, but is %X %X %X %X %X %X %X %X %X %X %X %X\n", mbr.BPB_reserved[0], mbr.BPB_reserved[1], mbr.BPB_reserved[2], mbr.BPB_reserved[3], mbr.BPB_reserved[4], mbr.BPB_reserved[5], mbr.BPB_reserved[6], mbr.BPB_reserved[7], mbr.BPB_reserved[8], mbr.BPB_reserved[9], mbr.BPB_reserved[10], mbr.BPB_reserved[11]);
-            break;
+            validMBR = 0;
         }
 
-        printf("MBR appears to be consistent.\n");
+        if (validMBR)
+        {
+            printf("MBR appears to be consistent.\n");
+        }
     }
 
     printf("\n\nEnd of Process.\n");
