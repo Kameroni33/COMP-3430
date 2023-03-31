f2bc51d0245f944f595e09b0abac7166e97b6480
kameron <khr675@gmail.com>
2023-03-31 13:16:28 -0500

    COMP 3430 (auto-commit at 13:16:28.25)

--- lab5/mmap.c
+++ lab5/mmap.c
@@ -47,7 +47,7 @@ int main(){
     printf("mmap returned error code %d\n", errno);
     exit(0);
   }
-  sprintf(syscmd, "pmap -XX %d", getpid());
+  sprintf(syscmd, "pmap -x -d -p %d", getpid());
   system(syscmd);
   printf("%s\n", (char *)mapped_file);
   close(fd);
