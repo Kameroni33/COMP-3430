f2e5a9c7aab9cd9377bde73c7cc81703ec0b2b94
kameron <khr675@gmail.com>
2023-04-06 15:17:33 -0500

    COMP 3430 (auto-commit at 15:17:33.25)

--- assignment3/fat32.c
+++ assignment3/fat32.c
@@ -3,8 +3,36 @@
 
 #define _FILE_OFFSET_BITS 64
 
+
+void info() {
+
+}
+
+void list() {
+
+}
+
+void get() {
+
+}
+
+
 int main(int argc, char *argv[]) {
-    printf("argv[0]: %s\nargc: %d\n\n", argv[0], argc);
+    printf("argc: %d\nargv[0]: %s\n\n", argv[0], argc);
+
+    if (argc >= 3 && argc <= 4) {
+        
+    }
+
+    else {
+        printf("\n\n  display information about a FAT32 drive\n\n");
+        printf("Usage:\n");
+        printf("  ./fat32 [imagefile] [command]\n\n");
+        printf("Available Commands:\n");
+        printf("  info   print out some general info about the drive\n");
+        printf("  list   print all files and directories on the frive\n");
+        printf("  get    fetch and return a file from the drive. Files will be copied to an ./output folder. Requires an additional parameter [file].\n\n")
+    }
 
     return 0;
 }
