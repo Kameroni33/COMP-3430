9c210fb33091c19f2ff0caca3c614a6bc727cafd
kameron <khr675@gmail.com>
2023-04-07 10:25:35 -0500

    COMP 3430 (auto-commit at 10:25:35.22)

--- assignment3/fat32.c
+++ assignment3/fat32.c
@@ -183,7 +183,7 @@ void printFileStructure(int drive, off_t addr, off_t fat, fat32BS bs) {
 
 
         uint32_t firstChar = (int)entry.dir_name[0] & 0xFF;
-        printf("\nFirst Character: 0x%x\n", firstChar);
+        // printf("\nFirst Character: 0x%x\n", firstChar);
         if (firstChar == 0xE5 || firstChar == 0x00) {
             printf("\n Empty Entry\n");
         }
