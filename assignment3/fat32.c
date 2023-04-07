721a356924a1e43b53855a39dd302f0ba0a5d9d4
kameron <khr675@gmail.com>
2023-04-07 10:32:26 -0500

    COMP 3430 (auto-commit at 10:32:26.18)

--- assignment3/fat32.c
+++ assignment3/fat32.c
@@ -189,7 +189,7 @@ void printFileStructure(int drive, off_t addr, off_t fat, fat32BS bs) {
         }
         for (int j = 0; j < 11; j++) {
             uint32_t currChar = (int)entry.dir_name[0] & 0xFF;
-            if (currChar <= 0x20 || currChar == 0x00) {
+            if (currChar <= 0x20 && currChar != 0x05) {
                 printf("WARNING: Invalid Entry Name Character\n");
             }
         }
