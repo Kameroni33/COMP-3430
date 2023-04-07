56137726c10e54e3231a1b4c055aae516fabd91e
kameron <khr675@gmail.com>
2023-04-06 19:42:04 -0500

    COMP 3430 (auto-commit at 19:42:04.27)

--- assignment3/fat32.c
+++ assignment3/fat32.c
@@ -5,6 +5,7 @@
 #define _FILE_OFFSET_BITS 64
 
 
+void printUsage();
 void info(char *drive);
 void list(char *drive);
 void get(char *drive, char *file);
@@ -23,26 +24,33 @@ int main(int argc, char *argv[]) {
                 get(argv[1], argv[3]);
             }
             else {
-                printf("Error: No file specified.\n");
+                printf("Error: no target file specified.\n");
+                printUsage();
             }
         }
         else {
-            printf("Error: Invalid command argument '%s'.\n", argv[2]);
+            printf("Error: invalid command argument '%s'.\n", argv[2]);
+            printUsage();
         }
     }
 
     else {
-        printf("\nProcess: fat32\n");
-        printf("  FAT32 drive reader\n\n");
-        printf("Usage:\n");
-        printf("  ./fat32 [imagefile] [command] {file}\n\n");
-        printf("Available Commands:\n");
-        printf("  info   print out some general info about the drive\n");
-        printf("  list   print all files and directories on the frive\n");
-        printf("  get    fetch and return a file from the drive. Files will be copied to ./output folder in current directory. Requires an additional argument {file}.\n\n");
+        printf("Error: incorrect usage '%s'.\n", argv[2]);
+        printUsage();
     }
 
     return 0;
+}
+
+void printUsage() {
+    printf("\nProcess: fat32\n");
+    printf("  FAT32 drive reader\n\n");
+    printf("Usage:\n");
+    printf("  ./fat32 [imagefile] [command] {file}\n\n");
+    printf("Available Commands:\n");
+    printf("  info   print out some general info about the drive\n");
+    printf("  list   print all files and directories on the frive\n");
+    printf("  get    fetch and return a file from the drive. Files will be copied to ./output folder in current directory. Requires an additional argument {file}.\n\n");
 }
 
 void info(char *drive) {
