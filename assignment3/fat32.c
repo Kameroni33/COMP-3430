c89e7b6275e58ded55499a9f13e9af87fb9a4d06
kameron <khr675@gmail.com>
2023-04-07 12:41:26 -0500

    COMP 3430 (auto-commit at 12:41:26.25)

--- assignment3/fat32.c
+++ assignment3/fat32.c
@@ -233,12 +233,6 @@ void printFileStructure(int drive, fat32BS bs, off_t fat, off_t cluster, int dep
                 calcFileName(entryName, fileName, 0);
                 printf("[system] %s\n", fileName);
             }
-            // else if ARCHIVE entry
-            else if (entry.dir_attr == ATTR_ARCHIVE) {
-                for (int k = 0; k < depth; k++) printf("-");
-                calcFileName(entryName, fileName, 0);
-                printf("[archive] %s\n", fileName);
-            }
             // else if VOLUME entry
             else if (entry.dir_attr == ATTR_VOLUME_ID) {
                 calcFileName(entryName, fileName, 0);
@@ -255,6 +249,12 @@ void printFileStructure(int drive, fat32BS bs, off_t fat, off_t cluster, int dep
                 // printf("<< newCluster: %u >>\n", newCluster);
 
                 printFileStructure(drive, bs, fat, newCluster, depth+1);
+            }
+            // else if ARCHIVE entry
+            else if (entry.dir_attr == ATTR_ARCHIVE) {
+                for (int k = 0; k < depth; k++) printf("-");
+                calcFileName(entryName, fileName, 0);
+                printf("[archive] %s\n", fileName);
             }
             // else FILE entry
             else {
