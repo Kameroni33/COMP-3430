4652c0c63d9074b24140897e77c5f158ecef02c6
kameron <khr675@gmail.com>
2023-04-06 19:42:38 -0500

    COMP 3430 (auto-commit at 19:42:38.25)

--- assignment3/fat32.c
+++ assignment3/fat32.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
     }
 
     else {
-        printf("Error: incorrect usage '%s'.\n");
+        printf("Error: incorrect usage.\n");
         printUsage();
     }
 
